eef9abaf39e0821ea510d8ae7ae8e774383fe075
Mathijs Frank <33691132+mathijs28498@users.noreply.github.com>
2026-01-03 00:57:57 +0000

    New code

--- src_test/main.c
+++ src_test/main.c
@@ -3,13 +3,16 @@
 #include <test_lib.h>
 #include <test_lib_i.h>
 
-int main()
+int main(int argc, const char *argv[])
 {
     TestStruct test_struct = {
-        .test_int = 400
-    };
+        .test_int = 400};
 
     printf("%s() - Value: %d\n", __func__, test_struct.test_int);
+    if (argc >= 3)
+    {
+        printf("%s() argc: %d - argv[1]: %s - argv[2]: %s\n", __func__, argc, argv[1], argv[2]);
+    }
 
     test_lib_print();
 
