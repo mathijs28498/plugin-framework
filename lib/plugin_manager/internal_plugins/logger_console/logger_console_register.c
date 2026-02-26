431b92dd23d5cf1a2a613540048c2ea33150dab0
Mathijs Frank <33691132+mathijs28498@users.noreply.github.com>
2026-02-26 17:31:12 +0000

    Added init to logger_console

--- lib/plugin_manager/internal_plugins/logger_console/logger_console_register.c
+++ lib/plugin_manager/internal_plugins/logger_console/logger_console_register.c
@@ -15,7 +15,6 @@ TODO("Remove this windows include")
 
 STATIC_ASSERT(LOG_LEVEL_MAX == LOGGER_CONSOLE_LOG_LEVEL_MAX, "log_level max_mismatch!");
 
-bool to_remove_done_alloc_console = false;
 LoggerInterface *get_interface(void)
 {
     static LoggerInterfaceContext context = {
@@ -32,34 +31,38 @@ LoggerInterface *get_interface(void)
     };
 
     TODO("Add this to an init function")
-#if IS_DEBUG && WINDOWS_GUI
-    if (!to_remove_done_alloc_console)
-    {
-        if (AllocConsole())
-        {
-            FILE *fDummy;
-
-            // Redirect standard streams to the new console "CONOUT$" and "CONIN$"
-            freopen_s(&fDummy, "CONOUT$", "w", stdout);
-            freopen_s(&fDummy, "CONOUT$", "w", stderr);
-            freopen_s(&fDummy, "CONIN$", "r", stdin);
-            HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-            if (hOut != INVALID_HANDLE_VALUE)
-            {
-                DWORD dwMode = 0;
-                if (GetConsoleMode(hOut, &dwMode))
-                {
-                    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
-                    SetConsoleMode(hOut, dwMode);
-                }
-            }
-        }
-        to_remove_done_alloc_console = true;
-    }
-#endif // #if IS_DEBUG && WINDOWS_GUI
-
     return &iface;
 }
 
 TODO("Figure out how to make this work with the macro")
 PLUGIN_REGISTER_INTERFACE(get_interface, LoggerInterface);
+
+int32_t init(LoggerInterfaceContext *context)
+{
+    (void)context;
+
+#if IS_DEBUG && WINDOWS_GUI
+    if (AllocConsole())
+    {
+        FILE *fDummy;
+
+        // Redirect standard streams to the new console "CONOUT$" and "CONIN$"
+        freopen_s(&fDummy, "CONOUT$", "w", stdout);
+        freopen_s(&fDummy, "CONOUT$", "w", stderr);
+        freopen_s(&fDummy, "CONIN$", "r", stdin);
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (hOut != INVALID_HANDLE_VALUE)
+        {
+            DWORD dwMode = 0;
+            if (GetConsoleMode(hOut, &dwMode))
+            {
+                dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+                SetConsoleMode(hOut, dwMode);
+            }
+        }
+    }
+#endif // #if IS_DEBUG && WINDOWS_GUI
+    return 0;
+}
+
+PLUGIN_REGISTER_INIT(init, LoggerInterfaceContext)
